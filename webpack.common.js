395eddd1a0976459b4aec057b8d30aca75fdb254
programmingwithfantom <mphosomo07@gmail.com>
2025-04-21 20:40:41 +0200

    fix: use 'template: ./src/template.html'

--- webpack.common.js
+++ webpack.common.js
@@ -7,11 +7,11 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Production",
+      template: "./src/template.html",
     }),
   ],
   output: {
-    filename: "main.bundle.js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
