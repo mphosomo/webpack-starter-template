3eb77b1f5cf6153a3bdf16b601b96e1f9d24ba41
programmingwithfantom <mphosomo07@gmail.com>
2025-04-21 17:17:32 +0200

    feat: seperate development and production configs using webpack-merge

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,20 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
+  entry: {
+    app: "./src/index.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      title: "Production",
     }),
   ],
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
   module: {
     rules: [
       {
@@ -34,4 +31,4 @@ module.exports = {
       },
     ],
   },
-}
+};
